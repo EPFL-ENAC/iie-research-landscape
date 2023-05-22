5c919e68dbeb333454dc37f16fdf520e560a27a3
Son Pham-Ba <son.phamba@epfl.ch>
2023-05-22 14:11:19 +0200

    feat: add data on keywords and parent categories

---
+++ scrapping/scripts/scrap_epfl_graph_keywords_categories.sql
@@ -0,0 +1,34 @@
+# Find unique keywords (concepts) and parent categories
+SELECT DISTINCT concept.PageTitle, category.CategoryName
+FROM Edges_N_Unit_N_Concept_T_Research edge
+JOIN Nodes_N_Unit unit USING (UnitID) # For filtering
+JOIN Edges_N_Category_N_Concept_T_Original concept USING (PageID)
+JOIN Nodes_N_Category category USING (CategoryID)
+WHERE unit.UnitPath LIKE '%> IIE%';
+
+
+# List all categories (filtered in IIE) and their parents
+WITH RECURSIVE filtered_categories AS (
+	# For testing:
+	# SELECT ChildCategoryID, ParentCategoryID
+	# FROM Edges_N_Category_N_Category category_edge
+	# WHERE category_edge.ChildCategoryID = "1000"
+
+	SELECT DISTINCT ChildCategoryID, ParentCategoryID
+	FROM Edges_N_Unit_N_Concept_T_Research edge
+	JOIN Nodes_N_Unit unit USING (UnitID) # For filtering
+	JOIN Edges_N_Category_N_Concept_T_Original concept USING (PageID)
+	JOIN Edges_N_Category_N_Category category_edge on concept.CategoryID = category_edge.ChildCategoryID
+	WHERE unit.UnitPath LIKE '%> IIE%'
+	
+	UNION ALL
+	
+	SELECT category_edge.ChildCategoryID, category_edge.ParentCategoryID
+	FROM Edges_N_Category_N_Category category_edge
+	JOIN filtered_categories ON filtered_categories.ParentCategoryID = category_edge.ChildCategoryID
+)
+# Convert IDs to names
+SELECT DISTINCT category.CategoryName, parent.CategoryName
+FROM filtered_categories edge
+JOIN Nodes_N_Category category ON edge.ChildCategoryID = category.CategoryID
+JOIN Nodes_N_Category parent ON edge.ParentCategoryID = parent.CategoryID;
